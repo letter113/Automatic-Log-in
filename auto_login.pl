2fcde7b592d9e24393e812ef4beaf944af9f59a3
unknown <ehhexxn@EV101F7459A0C4.eemea.ericsson.se>
2011-12-29 14:19:45 +0100

    first commit, logout unsolved and read from file not implemented

---
+++ auto_login.pl
@@ -0,0 +1,27 @@
+use HTTP::Request::Common qw(POST);
+use LWP::UserAgent;
+use LWP::ConnCache;
+
+sub log1{
+	($id,$pw) = @_;
+	$ua = LWP::UserAgent->new;
+	$ua->conn_cache(LWP::ConnCache->new());
+	my $req = POST 'http://bbs.fudan.sh.cn/bbs/login',
+	              [ "id"=>$id,"pw"=>$pw]; 
+	print "Try to login user: $id\n";
+	my$response = $ua->request($req);
+	$cookie = $response->header('Set-Cookie');
+	print "$cookie";
+	print $response->as_string;
+	print "Now log out\n";
+	$ua->conn_cache(LWP::ConnCache->new());
+  	$response = $ua->get("http://bbs.fudan.sh.cn/bbs/logout",
+  	Cookie=>$cookie);
+  	die "Error when logged out" unless $response.is_success;
+}
+
+sub handle{
+	$filename = $_;
+	open(my $handler, $filename);
+	
+}
